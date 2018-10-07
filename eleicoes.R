77d9210ea3af1347f394dec923f1d5dda3ffc58b
Helder Ribeiro <heldergr@users.noreply.github.com>
2018-10-07 20:51:16 -0300

    Update eleicoes.R

--- eleicoes.R
+++ eleicoes.R
@@ -1,32 +1,30 @@
-library(httr)
-library(XML)
 library(dplyr)
 library(jsonlite)
 
 Sys.setlocale("LC_ALL", "en_US.ISO8859-1")
 
-ap <- fromJSON("https://d2thur3zpak26x.cloudfront.net/2018/1-turno/presidente_brasil.json?t=1538953595061")
-m <- ap$municipios
+apuracao <- fromJSON("https://d2thur3zpak26x.cloudfront.net/2018/1-turno/presidente_brasil.json?t=1538953595061")
+print(apuracao$atualizacao$apuracao_total)
+municipios <- apuracao$municipios
 
-zoeira <- NULL
-for (m1 in m) {
-    nome <- m1$nm
-    c <- m1$c 
-    c$Municipio <- nome
+apuracaoMunicipio <- NULL
+for (municipio in municipios) {
+    c <- municipio$c 
+    c$Municipio <- municipio$nm
     c$v <- gsub("\\.", "", c$v)
     c$v <- as.numeric(c$v)
     c$Total <- sum(c$v)
-    if (is.null(zoeira)) {
-        zoeira <- c
+    if (is.null(apuracaoMunicipio)) {
+        apuracaoMunicipio <- c
     } else {
-        zoeira <- do.call(rbind, list(zoeira, c))
+        apuracaoMunicipio <- do.call(rbind, list(apuracaoMunicipio, c))
     }
 }
 
-zoeira <- mutate(zoeira, Perc = v / Total)
-zoeira <- arrange(zoeira, desc(Perc))
+apuracaoMunicipio <- mutate(apuracaoMunicipio, Perc = v / Total)
+apuracaoMunicipio <- arrange(apuracaoMunicipio, desc(Perc))
 
-noHaddad <- filter(zoeira, nc != "FERNANDO HADDAD")
+apuracaoSemHaddad <- filter(apuracaoMunicipio, nc != "FERNANDO HADDAD")
 
-View(zoeira)
-View(noHaddad)
+View(apuracaoMunicipio)
+View(apuracaoSemHaddad)
